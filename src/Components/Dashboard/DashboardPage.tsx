ab96787d2b64cdc0189d813305fc8fa3bf7adf11
Julia Oleksiuk <julkaoleksiuk26@gmail.com>
2023-10-25 23:33:23 +0200

    Fixed page layout

--- src/Components/Dashboard/DashboardPage.tsx
+++ src/Components/Dashboard/DashboardPage.tsx
@@ -1,7 +1,14 @@
 import { Box, Toolbar, Container } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 import { Colors } from '@/constants/Colors'
+import styled from 'styled-components'
 
+export const PageContainerStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 100px;
+`
 export default function DashboardPage() {
     return (
         <Box
@@ -10,11 +17,12 @@ export default function DashboardPage() {
                 backgroundColor: Colors.pageBackground,
                 flexGrow: 1,
                 height: '100vh',
-                overflow: 'auto',
             }}
         >
             <Toolbar />
-            <Outlet />
+            <PageContainerStyled>
+                <Outlet />
+            </PageContainerStyled>
         </Box>
     )
 }
